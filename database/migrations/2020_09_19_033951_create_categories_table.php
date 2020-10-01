18a1f34beebd06f69ac1c429fb01d01fd08c0531
nguyentranduythuan <nguyentranduythuan@gmail.com>
2020-10-01 08:59:24 +0700

    edit default value in categories

--- database/migrations/2020_09_19_033951_create_categories_table.php
+++ database/migrations/2020_09_19_033951_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->longText('description')->nullable()->default('NULL');
+            $table->string('description')->nullable()->default('255');
             $table->string('image_up')->nullable();
             $table->string('image_link')->nullable();
             $table->timestamps();
