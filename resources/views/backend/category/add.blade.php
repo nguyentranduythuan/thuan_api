2ba0b338c6c5fc1acfe20aff9d0d3106b99f943a
nguyentranduythuan <nguyentranduythuan@gmail.com>
2020-10-01 10:07:27 +0700

    aaaaaa

--- resources/views/backend/category/add.blade.php
+++ resources/views/backend/category/add.blade.php
@@ -28,6 +28,7 @@
 	      	<div class="card-body">
 	      		<form action="{{ route('v1.category.store') }}" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 	      			@csrf
+	      			{{ method_field('PUT') }}
 	      			<div class="row">
 			          	<div class="col-md-12">
 				            <div class="form-group">
